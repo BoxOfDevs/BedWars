9295a216275d0c76e8f010e5c09d5eda91d15d8c
Taki_In_Training <Taki21@users.noreply.github.com>
2017-04-08 19:25:15 -0400

    maybe i lied... how do i know all dis info...

--- src/Taki21/Main.php
+++ src/Taki21/Main.php
@@ -2,4 +2,44 @@
 
 namespace Taki21;
 
-// cya soon code
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Server;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\Listener;
+use pocketmine\math\Vector3;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat as C;
+use pocketmine\utils\Config;
+
+class Main extends Pluginbase implements Listener { // pretty sure this is how u do it...
+  
+  public function onLoad(){
+    // cya later code
+  }
+  
+  public function onEnable(){
+    // cya later code
+  }  
+  
+  public function onCommand(CommandSender $s, Command $cmd, $label, array $args){ //wut
+    if(strtolower($cmd->getName() == "bw")){
+      if(!$s instanceof Player){
+        $s->sendMessage(C::RED."This Command Can Only by used by Players");
+      }else{
+        //blah
+      }  
+    }
+    switch(strtolower($args[0])){
+      case "":
+      break;
+    }
+    return true;
+  }
+  
+  public function onDisable(){
+    // cya later code
+  }  
+  
+}
